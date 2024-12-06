7cb2bfd24ab02e24fb250104d27a597cc66d0ae3
rahulp1262003 <prajapatirahul1262003@gmail.com>
2024-12-06 09:58:55 +0530

    update little style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,33 +20,33 @@ function Navbar() {
 
                     <div className='mx-4 mt-3 rounded-xl min-w-max flex  items-start flex-col' >
                         {/* Trends */}
-                        <NavLink to="/" className={({isActive})=>(isActive ? 'flex px-6 py-4 my-1 w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4 my-1 w-full hover:bg-blue-200 hover:text-black hover:rounded-2xl')}><span className='pr-2'><IoTrendingUpOutline size={25} /></span>Trends</NavLink>
+                        <NavLink to="/" className={({isActive})=>(isActive ? 'flex px-6 py-4 my-1 w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4 my-1 w-full hover:bg-blue-50 hover:text-black hover:rounded-2xl')}><span className='pr-2'><IoTrendingUpOutline size={25} /></span>Trends</NavLink>
 
                         {/* Explore */}
-                        <NavLink to="/explore" className={({isActive})=>(isActive ? 'flex px-6 py-4 my-1 w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4 my-1 w-full hover:bg-blue-200 hover:text-black hover:rounded-2xl')}><span className='pr-2'><IoBagOutline size={25} /></span>Explore</NavLink>
+                        <NavLink to="/explore" className={({isActive})=>(isActive ? 'flex px-6 py-4 my-1 w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4 my-1 w-full hover:bg-blue-50 hover:text-black hover:rounded-2xl')}><span className='pr-2'><IoBagOutline size={25} /></span>Explore</NavLink>
 
                         {/* Food */}
-                        <NavLink to="/food" className={({isActive})=>(isActive ? 'flex px-6 py-4 my-1 w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4 my-1 w-full hover:bg-blue-200 hover:text-black hover:rounded-2xl')}><span className='pr-2'><PiHamburger size={25} /></span>Food</NavLink>
+                        <NavLink to="/food" className={({isActive})=>(isActive ? 'flex px-6 py-4 my-1 w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4 my-1 w-full hover:bg-blue-50 hover:text-black hover:rounded-2xl')}><span className='pr-2'><PiHamburger size={25} /></span>Food</NavLink>
 
                         {/* Cloths */}
-                        <NavLink to="/cloths" className={({isActive})=>(isActive ? 'flex px-6 py-4 my-1 w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4 my-1 w-full hover:bg-blue-200 hover:text-black hover:rounded-2xl')}><span className='pr-2'><PiTShirt size={25} /></span>Cloths</NavLink>
+                        <NavLink to="/cloths" className={({isActive})=>(isActive ? 'flex px-6 py-4 my-1 w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4 my-1 w-full hover:bg-blue-50 hover:text-black hover:rounded-2xl')}><span className='pr-2'><PiTShirt size={25} /></span>Cloths</NavLink>
 
                         {/* Electronics */}
-                        <NavLink to="/electronics" className={({isActive})=>(isActive ? 'flex px-6 py-4 my-1 w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4 my-1 w-full hover:bg-blue-200 hover:text-black hover:rounded-2xl')}><span className='pr-2'><BsTv size={25} /></span>Electronics</NavLink>
+                        <NavLink to="/electronics" className={({isActive})=>(isActive ? 'flex px-6 py-4 my-1 w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4 my-1 w-full hover:bg-blue-50 hover:text-black hover:rounded-2xl')}><span className='pr-2'><BsTv size={25} /></span>Electronics</NavLink>
                     </div>
 
                     {/* Orders and Cart */}
                     <div className='mx-4 mb-10 mt-10 bg-blue-100 rounded-2xl min-w-max flex items-start flex-col' >
                         {/* Orders */}
-                        <NavLink to="/orders" className={({isActive})=>(isActive ? 'flex px-6 py-4 my-1 w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4 my-1 w-full hover:bg-blue-200 hover:text-black hover:rounded-2xl')} ><span className='pr-2' ><LuListChecks size={25} /></span>Orders</NavLink>
+                        <NavLink to="/orders" className={({isActive})=>(isActive ? 'flex px-6 py-4  w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4  w-full hover:bg-blue-200 hover:text-black hover:rounded-2xl')} ><span className='pr-2' ><LuListChecks size={25} /></span>Orders</NavLink>
 
                         {/* Cart */}
-                        <NavLink to="/cart" className={({isActive})=>(isActive ? 'flex px-6 py-4 my-1 w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4 my-1 w-full hover:bg-blue-200 hover:text-black hover:rounded-2xl')}><span className='pr-2'><PiShoppingCart size={25} /></span>Cart</NavLink>
+                        <NavLink to="/cart" className={({isActive})=>(isActive ? 'flex px-6 py-4  w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4  w-full hover:bg-blue-200 hover:text-black hover:rounded-2xl')}><span className='pr-2'><PiShoppingCart size={25} /></span>Cart</NavLink>
                     </div>
 
                     {/* Settings */}
                     <div className="flex m-3 rounded-2xl">
-                        <NavLink to="/settings" className={({isActive})=>(isActive ? 'flex px-6 py-4 my-1 w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4 my-1 w-full hover:bg-blue-200 hover:text-black hover:rounded-2xl')}><span className='pr-2 flex'><IoSettingsOutline size={25} /></span>Setting</NavLink>
+                        <NavLink to="/settings" className={({isActive})=>(isActive ? 'flex px-6 py-4 my-1 w-full bg-blue-600 text-white rounded-2xl' : 'flex px-6 py-4 my-1 w-full hover:bg-blue-50 hover:text-black hover:rounded-2xl')}><span className='pr-2 flex'><IoSettingsOutline size={25} /></span>Setting</NavLink>
                     </div>
                 </div>
             </aside>
